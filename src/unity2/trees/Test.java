af09127b6def560ee14f8805e8dc443bce3ccd18
MiPc <MiPc@MayeYandar>
2016-04-25 21:25:08 -0500

    Unity2

--- src/unity2/trees/Test.java
+++ src/unity2/trees/Test.java
@@ -10,24 +10,30 @@ package unity2.trees;
  * @author AULA1
  */
 public class Test {
-    
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         Tree t = new Tree();
-        int values[] = {8,4,16,2,6,12,20,1,10,14,13,15};
-        for (int i=0; i<values.length; i++){
+        int values[] = {8, 4, 16, 2, 6, 12, 20, 1, 10, 14, 13, 15};
+        for (int i = 0; i < values.length; i++) {
             t.add(values[i]);
         }
 
-        int v=30; 
-        System.out.print("Path to" +v+ ":");
+        int v = 30;
+        System.out.print("Path to" + v + ":");
         t.printPath(v);
-        
-        if(t.isLeaf(v)){
-            System.out.println(" "+v+ "is a leaf");
-        }else{
-            System.out.println(" "+v+ "doesn't a leaf");
-        }
-    }
-    
-}
 
+        if (t.isLeaf(v)) {
+            System.out.println(" " + v + "is a leaf");
+        } else {
+            System.out.println(" " + v + "doesn't a leaf");
+        }
+
+        t.Printleafs();
+        t.Max();
+        t.Count();
+        t.Min();
+        
+        
+    }
+
+}
